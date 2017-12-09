4fda2df1a918e3d7597a5e9e660c168ff78af9cc
Rafael Dias <rafaeldiasmello@gmail.com>
2017-12-09 11:06:05 -0200

    Termina menu.

---
+++ app/src/test/java/meu_curriculo/av2/dias/rafael/meucurriculo/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package meu_curriculo.av2.dias.rafael.meucurriculo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
